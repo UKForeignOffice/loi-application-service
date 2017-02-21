bd1cdc869e0e8cd9ced102df49398b321965fa4d
Sean Mulvenna <S.Mulvenna@kainos.com>
2017-02-21 09:21:22 +0000

    fixing error for non js address lookup

--- api/controllers/UsersAddressDetailsController.js
+++ api/controllers/UsersAddressDetailsController.js
@@ -231,7 +231,22 @@ var UsersAddressDetailsCtrl = {
                 };
 
                 return res.view("applicationForms/address/UKAddressSelect.ejs", options);
-            });
+            },
+              function(err)
+              {
+                console.log(err)
+                req.flash('error', 'Please enter your address manually');
+                var options = {
+                  user_data: HelperService.getUserData(req, res),
+                  error_report: req.flash('error'),
+                  address_type: address_type,
+                  user_address: req.session.user_addresses[address_type],
+                  addresses: false,
+                  postcode: '',
+                  summary: req.session.summary
+                };
+                return  res.view("applicationForms/address/UKAddressSelect.ejs",options);
+              });
         }
     },
     /**
@@ -303,10 +318,7 @@ var UsersAddressDetailsCtrl = {
               function(err)
               {
                 console.log(err)
-                var return_error = 'Enter a valid postcode';
-                var addresses = false;
                 return res.json({error:'Please enter your address manually'});
-                //return {addresses: addresses, return_error: return_error};
               }
             );
         }
