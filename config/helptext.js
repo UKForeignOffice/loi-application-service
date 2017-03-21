d6679426d63cb48e413e2c2c1f7d4d57ac6dad7f
simonmoore100 <s.moore@kainos.com>
2017-03-21 10:14:07 +0000

    Add piwik events, add different helptext style and move helptext content out into a config file

---
+++ config/helptext.js
@@ -0,0 +1,16 @@
+module.exports = {
+  sae: {
+    line1: "Stamped self-addressed envelope instructions",
+    line2: "1. Add stamps on to a large envelope - enough to cover posting your documents back:",
+    line3: "we recommend you go to your local post office who will help you buy the right postage",
+    line4: "the cost varies depending on the size, weight and your delivery preferences - ",
+    line5: "use an envelope which will hold your documents unfolded - A4 or larger, classified as a 'Large Letter' by Royal Mail",
+    line6: "2. Write your return address on the envelope - we'll use this envelope to send your documents back to you when they've been legalised.",
+    line7: "3. Put this stamped self-addressed envelope together with your documents in a separate envelope addressed to the Legalisation Office.",
+    linkURL: "http://www.royalmail.com/price-finder",
+    linkText: "check the latest Royal Mail pricing"
+  },
+  courier: {
+    line1: "Your documents will be couriered in a waterproof envelope. If you want additional protection, when you send us your application include an envelope which will hold your documents unfolded (typically A4 size or larger)"
+  }
+};
