e68d2e85d29ba54b73892cf29c9087d8cc7045df
simonmoore100 <s.moore@kainos.com>
2017-07-20 08:35:40 +0100

    Add SQL to ensure Home Office docs not signed are subject to certification.

---
+++ database_change_log/fco0356_certify_ho_docs.sql
@@ -0,0 +1,3 @@
+update "AvailableDocuments"
+set eligible_check_option_1 = 'Your original *replaceme* not signed by an official of the issuing authority <span>certification required</span>'
+where html_id = 'home-office-document';
