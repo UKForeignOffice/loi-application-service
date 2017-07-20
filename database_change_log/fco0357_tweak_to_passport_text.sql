b17dca94a6e464bcb157154d85559b84cfe04732
simonmoore100 <s.moore@kainos.com>
2017-07-20 08:44:51 +0100

    Add SQL to update passport acceptance text

---
+++ database_change_log/fco0357_tweak_to_passport_text.sql
@@ -0,0 +1,3 @@
+update "AvailableDocuments"
+set accept_text = 'We can only legalise a certified copy of your *replaceme*. The copy must include the page which displays your digital or link signature. We cannot legalise the original document. Please confirm that you will send us:'
+where html_id = 'passport';
