dc88cd1eba3e58b94312cd6bdc60142bcd4216a1
Conor McGandy <c.mcgandy@kainos.com>
2018-08-30 16:17:05 +0100

    html_id updated to all lowercase, other columns now includes the word 'document' as confirmed on KIMFCO0585

--- database_change_log/FCO0585_CRB_to_DBS.sql
+++ database_change_log/FCO0585_CRB_to_DBS.sql
@@ -1,3 +1,3 @@
 UPDATE public."AvailableDocuments"
-SET html_id='disclosure-and-Barring-Service-dbs',doc_title = 'Disclosure and Barring Service (DBS)',doc_title_start = 'Disclosure and Barring Service (DBS)',doc_title_mid = 'Disclosure and Barring Service (DBS)'
+SET html_id='disclosure-and-barring-service-dbs-document',doc_title = 'Disclosure and Barring Service (DBS) document',doc_title_start = 'Disclosure and Barring Service (DBS) document',doc_title_mid = 'Disclosure and Barring Service (DBS) document'
 WHERE doc_id = 216
