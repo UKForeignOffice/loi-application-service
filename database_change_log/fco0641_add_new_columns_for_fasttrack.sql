330c9a040c2a352afd5b0d085be4e6a157958486
Martin Carson <M.Carson@kainos.com>
2019-02-15 10:57:45 +0000

    Added quotation marks to make the column name work with caps

--- database_change_log/fco0641_add_new_columns_for_fasttrack.sql
+++ database_change_log/fco0641_add_new_columns_for_fasttrack.sql
@@ -2,7 +2,7 @@
 --  Connect to FCO-LOI-Service and run the following:
 --
 
-ALTER TABLE public."Application" ADD COLUMN doc_reside_EU boolean;
+ALTER TABLE public."Application" ADD COLUMN "doc_reside_EU" boolean;
 
 ALTER TABLE public."Application" ADD COLUMN residency boolean;
 
