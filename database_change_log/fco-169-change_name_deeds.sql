5a525ade99882d7c3e4a8f4acbf3ba279ec901f7
Martin Carson <martinc@kainos.com>
2020-04-15 22:07:08 +0100

    added fixes to scripts

--- database_change_log/fco-169-change_name_deeds.sql
+++ database_change_log/fco-169-change_name_deeds.sql
@@ -12,22 +12,22 @@ ADD COLUMN eligible_check_option_6 text;
 
 
 --update first point
-UPDATE "AvailableDocuments" SET eligible_check_option_1 = 'Original *replaceme* signed, certified and dated by a UK solicitor or notary public' WHERE doc_id = '207';
+UPDATE "AvailableDocuments" SET eligible_check_option_1 = 'An original *replaceme* signed, certified and dated by a UK solicitor or notary public' WHERE doc_id = '207';
 
 --update 2nd point
-UPDATE "AvailableDocuments" SET eligible_check_option_2 = 'Original *replaceme* produced by a council or court, signed with an original wet ink signature by a council or court official.' WHERE doc_id = '207';
+UPDATE "AvailableDocuments" SET eligible_check_option_2 = 'An original *replaceme* produced by a council or court, signed with an original wet ink signature by a council or court official' WHERE doc_id = '207';
 
 --update 3rd point
-UPDATE "AvailableDocuments" SET eligible_check_option_3 = 'Original *replaceme* not signed, certified and dated by a UK solicitor or notary public' WHERE doc_id = '207';
+UPDATE "AvailableDocuments" SET eligible_check_option_3 = 'An original *replaceme* not signed, certified and dated by a UK solicitor or notary public  <span>certification required</span>' WHERE doc_id = '207';
 
 --update 4th point
-UPDATE "AvailableDocuments" SET eligible_check_option_4 = 'Original *replaceme* produced by a council or court not signed by a council or court official' WHERE doc_id = '207';
+UPDATE "AvailableDocuments" SET eligible_check_option_4 = 'An original *replaceme* produced by a council or court not signed by a council or court official  <span>certification required</span>' WHERE doc_id = '207';
 
 --update 5th point
-UPDATE "AvailableDocuments" SET eligible_check_option_5 = 'Photocopy of your *replaceme*' WHERE doc_id = '207';
+UPDATE "AvailableDocuments" SET eligible_check_option_5 = 'A photocopy of your *replaceme*  <span>certification required</span>' WHERE doc_id = '207';
 
 --update 6th point
-UPDATE "AvailableDocuments" SET eligible_check_option_6 = 'A print out of your *replaceme* which has been produced as a PDF/electronic document. The document may contain an electronic/digital signature or seal.' WHERE doc_id = '207';
+UPDATE "AvailableDocuments" SET eligible_check_option_6 = 'A print out of your *replaceme* which has been produced as a PDF/electronic document. The document may contain an electronic/digital signature or seal  <span>certification required</span>' WHERE doc_id = '207';
 
 
 
