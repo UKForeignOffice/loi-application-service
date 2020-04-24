d617d3cda031bf8199ea17bde068c4a961f2a4b2
Simon Moore <s.moore@kainos.com>
2020-04-24 14:47:52 +0100

    Add form validation

--- api/controllers/AdditionalPaymentsController.js
+++ api/controllers/AdditionalPaymentsController.js
@@ -3,6 +3,22 @@
  * @module Controller AdditionalPaymentsController
  */
 
+function formatMoney (num) {
+  let numeral = require('numeral');
+  return numeral(num).format('0.00');
+}
+
+function validateEmail (email) {
+  let isemail = require('isemail');
+  if (typeof email !== 'undefined') {
+    return isemail.validate(email)
+  } else return false
+}
+
+function validateCost (cost) {
+  return typeof cost !== 'undefined';
+}
+
 var additionalPaymentsController = {
 
   /**
@@ -13,13 +29,36 @@ var additionalPaymentsController = {
    * @return res.redirect
    */
   start: function (req, res) {
-    return res.view('additionalPayments/start.ejs')
+    let errors = [];
+    return res.view('additionalPayments/start.ejs',{
+      errors:errors
+    })
   },
 
   confirm: function (req, res) {
-
     try {
       if (req.method === 'POST'){
+        let errors = [];
+
+        if (!validateEmail(req.body.email)){
+          errors.push({msg:'Please enter a correctly formatted email address.'})
+        }
+
+        if (!validateCost(req.body.cost)){
+          errors.push({msg:'Please enter a payment amount'})
+        }
+
+        let sess = req.session;
+        sess.additionalPayments = {};
+        sess.additionalPayments.cost = formatMoney(req.body.cost);
+        sess.additionalPayments.email = req.body.email;
+
+        if (errors.length > 0){
+          return res.view('additionalPayments/start.ejs', {
+            errors:errors
+          })
+        }
+
         let paymentUrl = sails.config.additionalPayments.additionalPaymentStartPageUrl;
         return res.redirect(307, paymentUrl)
       } else {
