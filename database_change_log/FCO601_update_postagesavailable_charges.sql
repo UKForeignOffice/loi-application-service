d6d42053325ce59e5149d7d4f65957da94164028
Benjamin Dickey <benjamindickey@hotmail.co.uk>
2021-02-16 14:24:31 +0000

    Correct IDs in update

--- database_change_log/FCO601_update_postagesavailable_charges.sql
+++ database_change_log/FCO601_update_postagesavailable_charges.sql
@@ -1,6 +1,6 @@
 -- EU
-UPDATE "PostagesAvailable" set "price" = 15.00 where id = 1;
+UPDATE "PostagesAvailable" set "price" = 15.00 where id = 9;
 -- international
-UPDATE "PostagesAvailable" set "price" = 20.00 where id = 2;
+UPDATE "PostagesAvailable" set "price" = 20.00 where id = 10;
 -- UK
 UPDATE "PostagesAvailable" set "price" = 5.00 where id = 8;
