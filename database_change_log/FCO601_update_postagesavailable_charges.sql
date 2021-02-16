12f41cdc4e421eb12340eb84b86bb1e521e0eaf4
Benjamin Dickey <benjamindickey@hotmail.co.uk>
2021-02-16 12:11:54 +0000

    Update postal charges

---
+++ database_change_log/FCO601_update_postagesavailable_charges.sql
@@ -0,0 +1,6 @@
+-- EU
+UPDATE "PostagesAvailable" set "price" = 15.00 where id = 1;
+-- international
+UPDATE "PostagesAvailable" set "price" = 20.00 where id = 2;
+-- UK
+UPDATE "PostagesAvailable" set "price" = 5.00 where id = 8;
