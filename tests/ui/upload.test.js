5a7ae7e5f8e97dd90706d219ad89790784115141
Richard Bray <richardbray89@gmail.com>
2021-07-06 13:35:50 +0100

    test: fix ui related tests

--- tests/ui/upload.test.js
+++ tests/ui/upload.test.js
@@ -1,45 +1,71 @@
-const { screen } = require('@testing-library/dom');
-const { default: userEvent } = require('@testing-library/user-event')
-const path = require('path')
-const ejs = require('ejs')
+const { screen } = require("@testing-library/dom");
+const { default: userEvent } = require("@testing-library/user-event");
+const path = require("path");
+const ejs = require("ejs");
 
 const loadDom = async ({ filePath, data }) => {
   const targetFile = path.resolve(__dirname, filePath);
-  document.body.innerHTML = await ejs.renderFile(targetFile, data, { async: true });
-}
+  document.body.innerHTML = await ejs.renderFile(targetFile, data, {
+    async: true,
+  });
+};
 
-describe('File upload page', () => {
-  beforeEach(() => loadDom({
-    filePath: '../../views/eApostilles/uploadFiles.ejs',
-    data: { session: '', user_data: {}, uploadedFiles: [] }
-  }))
+const initialPageData = {
+  session: "",
+  formInputName: "documents",
+  error_report: false,
+  user_data: {
+    loggedIn: false,
+    user: {
+      id: 123,
+    },
+  },
+  uploadedFiles: [],
+  errors: [],
+  fileCountErrorMsg: false,
+  generalMessage: null,
+};
 
-  describe('No JavaScript', () => {
-    it('should render a main heading', () => {
-      const heading = screen.queryByText('Add your PDFs')
-      expect(heading).toBeInTheDocument()
+describe("File upload page", () => {
+  beforeEach(() =>
+    loadDom({
+      filePath: "../../views/eApostilles/uploadFiles.ejs",
+      data: initialPageData,
     })
+  );
 
-    it('should upload files when the file input changes', done => {
-      const form = screen.queryByTestId('upload-form');
-      const input = screen.queryByLabelText('Upload files')
-      form.addEventListener('submit', e => {
-        e.preventDefault()
-        expect(e.target.action.endsWith('/upload-file-handler')).toBeTrue()
+  describe("Without JavaScript", () => {
+    it("should render a main heading", () => {
+      const heading = screen.queryByText("Add your PDFs");
+      expect(heading).toBeInTheDocument();
+    });
+
+    it("should upload files when the file input changes", (done) => {
+      const form = screen.queryByTestId("upload-form");
+      const input = screen.queryByLabelText("Upload files");
+      form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        expect(e.target.action.endsWith("/upload-file-handler/123")).toBeTrue();
         done();
       });
-      const file = new File(["hello"], "hello.pdf", { type: "application/pdf" })
-      userEvent.upload(input, file, { changeInit: true })
-      const submitButton = screen.queryByText('Upload files', { selector: 'button' })
+      const file = new File(["hello"], "hello.pdf", {
+        type: "application/pdf",
+      });
+      userEvent.upload(input, file, { changeInit: true });
+      const submitButton = screen.queryByText("Upload", {
+        selector: "button",
+      });
       userEvent.click(submitButton);
-    })
+    });
   });
 
-  describe('With JavaScript', () => {
-    it('should show a drag and drop prompt', async () => {
-      require(process.cwd() + '/assets/js/multi-file-upload')
-      const prompt = await screen.findByText('Drag and drop PDF files here or')
-      expect(prompt).toBeInTheDocument();
-    })
-  });
-})
+  // TODO drag and drop functionality omitted for MVP
+  //
+  // describe("With JavaScript", () => {
+  //   it("should show a drag and drop prompt", async () => {
+  //     require(process.cwd() + "/assets/js/multi-file-upload");
+  //     const prompt = await screen.findByText("Drag and drop PDF files here or");
+  //     expect(prompt).toBeInTheDocument();
+  //   });
+  // });
+});
