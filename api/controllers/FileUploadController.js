7c19374209458eb33e6334875b91063e2544de4c
Richard Bray <richardbray89@gmail.com>
2021-07-08 17:04:48 +0100

    fix(EIA-89): use multer fileFilter to check for errors

--- api/controllers/FileUploadController.js
+++ api/controllers/FileUploadController.js
@@ -2,6 +2,8 @@ const multer = require("multer");
 const AWS = require("aws-sdk");
 const s3 = new AWS.S3();
 const multerS3 = require("multer-s3");
+const fs = require("fs");
+
 const MAX_FILES = 20;
 const FORM_INPUT_NAME = "documents";
 const ONE_HUNDRED_MEGABYTES = 100 * 1_000_000;
@@ -19,11 +21,32 @@ AWS.config.update({
   region: "eu-west-2",
 });
 
-// TODO have similar filename locally and on S3
-const storeLocally = multer.memoryStorage();
+// TODO refactor to private methods
+const s3BucketName = sails.config.eAppS3Vals.s3_bucket;
+const inDevEnvironment = process.env.NODE_ENV === "development";
+
+const storeLocally = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename(req, file, cb) {
+    const storageName = `${Date.now().toString()}-${file.originalname}`;
+    // TODO needs to be seperate function
+    uploadCache.uploadedFiles = [
+      ...uploadCache.uploadedFiles,
+      {
+        filename: file.originalname,
+        storageName,
+      },
+    ];
+
+    cb(null, storageName);
+  },
+});
+
 const storeInS3 = multerS3({
   s3,
-  bucket: sails.config.eAppS3Vals.s3_bucket,
+  bucket: s3BucketName,
   metadata: (req, _, cb) =>
     cb(null, {
       userEmail: req.session.email,
@@ -33,30 +56,64 @@ const storeInS3 = multerS3({
     cb(null, `${Date.now().toString()}-${file.originalname}`),
 });
 
+function checkTypeSizeAndDuplication(req, file, cb) {
+  let errors = [];
+  if (file.mimetype !== "application/pdf") {
+    errors.push(
+      `The file is in the wrong format. Only .pdf files are allowed.`
+    );
+  }
+
+  if (file.size > MAX_BYTES_PER_FILE) {
+    errors.push(
+      `The file is too large (${formatFileSizeMb(
+        file.size
+      )}). The maximum size allowed is ${formatFileSizeMb(
+        MAX_BYTES_PER_FILE,
+        0
+      )}`
+    );
+  }
+
+  if (
+    uploadCache.uploadedFiles.find(
+      (existing) => existing.filename === file.originalname
+    )
+  ) {
+    errors.push(
+      `You\'ve already uploaded a file named ${file.originalname}. Each file in an application must have a unique name.`
+    );
+  }
+
+  if (errors.length > 0) {
+    uploadCache.uploadedFiles.forEach((uploadedFile, index) => {
+      if (uploadedFile.filename === file.originalname) {
+        uploadCache.uploadedFiles[index].errors = errors;
+        uploadCache.errors.push(uploadCache.uploadedFiles[index]);
+      }
+    });
+    cb(null, false); // Reject file upload
+  } else {
+    cb(null, true); // Accept file upload
+  }
+}
+
 const upload = multer({
-  storage: process.env.NODE_ENV === "development" ? storeLocally : storeInS3,
+  storage: inDevEnvironment ? storeLocally : storeInS3,
+  fileFilter: checkTypeSizeAndDuplication,
   limits: {
     files: MAX_FILES,
   },
 }).array(FORM_INPUT_NAME);
 
-const responseSuccess = (file) => ({
-  filename: file.originalname,
-});
-
-const responseError = (file, errors) => ({
-  errors,
-  filename: file.originalname,
-});
-
 const formatFileSizeMb = (bytes, decimalPlaces = 1) => {
   return `${(bytes / 1_000_000).toFixed(decimalPlaces)}Mb`;
 };
 
-
 const FileUploadController = {
   uploadFilesPage(req, res) {
     const userData = HelperService.getUserData(req, res);
+
     uploadCache.uploadedFiles = req.session.eApp.uploadedFileData;
 
     if (!userData.loggedIn) {
@@ -90,69 +147,13 @@ const FileUploadController = {
     } else if (err) {
       sails.log.error(err);
       res.serverError(err);
-    } else {
-      req.files.forEach((file) =>
-        FileUploadController._checkIndividualFilesForErrors(file)
-      );
     }
+
     FileUploadController._updateSessionWithUploadData(req, res);
   },
 
-  _checkIndividualFilesForErrors(file) {
-    // non-JS form post - one or many files sent
-    const fileData = FileUploadController._checkTypeSizeAndDuplication(
-      file,
-      uploadCache.uploadedFiles
-    );
-
-    if (fileData.errors) {
-      uploadCache.errors.push(fileData);
-    } else {
-      uploadCache.uploadedFiles.push(fileData);
-      sails.log.info(`File uploaded successfully: `, fileData);
-    }
-  },
-
-  _checkTypeSizeAndDuplication(file, uploadedFiles) {
-    let fileData;
-    let errors = [];
-    if (file.mimetype !== "application/pdf") {
-      errors.push(
-        `The file is in the wrong format. Only .pdf files are allowed.`
-      );
-    }
-
-    if (file.size > MAX_BYTES_PER_FILE) {
-      errors.push(
-        `The file is too large (${formatFileSizeMb(
-          file.size
-        )}). The maximum size allowed is ${formatFileSizeMb(
-          MAX_BYTES_PER_FILE,
-          0
-        )}`
-      );
-    }
-
-    if (
-      uploadedFiles.find((existing) => existing.filename === file.originalname)
-    ) {
-      errors.push(
-        `You\'ve already uploaded a file named ${file.originalname}. Each file in an application must have a unique name`
-      );
-    }
-
-    if (errors.length > 0) {
-      fileData = responseError(file, errors);
-    } else {
-      fileData = responseSuccess(file);
-    }
-
-    return fileData;
-  },
-
   deleteFileHandler(req, res) {
-    let uploadedFiles =
-      uploadCache && uploadCache.uploadedFiles;
+    let uploadedFiles = uploadCache && uploadCache.uploadedFiles;
 
     if (!req.body.delete) {
       return res.badRequest("Item to delete wasn't specified");
@@ -162,14 +163,42 @@ const FileUploadController = {
       return res.notFound("Item to delete wasn't found");
     }
 
-    uploadCache.uploadedFiles = uploadedFiles.filter((file) => {
-      if (file.filename === req.body.delete) {
-        sails.log.info(`File deleted: `, req.body.delete);
+    uploadCache.uploadedFiles = uploadedFiles.filter((uploadedFile) => {
+      const fileToDeleteFound = uploadedFile.filename === req.body.delete;
+
+      if (fileToDeleteFound && inDevEnvironment) {
+        FileUploadController._deleteFileLocally(uploadedFile);
       }
-      return file.filename !== req.body.delete;
+
+      if (fileToDeleteFound && !inDevEnvironment) {
+        FileUploadController._deleteFileFromS3(uploadedFile);
+      }
+
+      return uploadedFile.filename !== req.body.delete;
     });
 
     FileUploadController._updateSessionWithUploadData(req, res);
+  },
+
+  _deleteFileLocally(uploadedFile) {
+    fs.unlink(uploadedFile.key, (err) => {
+      if (err) {
+        sails.log.error(err, err.stack);
+      }
+      sails.log.info(`File deleted: `, uploadedFile.filename);
+    });
+  },
+
+  _deleteFileFromS3(uploadedFile) {
+    const params = { Bucket: s3BucketName, Key: uploadedFile.key };
+
+    s3.deleteObject(params, (err) => {
+      if (err) {
+        sails.log.error(err, err.stack);
+      } else {
+        sails.log.info(`File deleted from S3: `, uploadedFile.filename);
+      }
+    });
   },
 
   _updateSessionWithUploadData(req, res) {
