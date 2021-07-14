93cb2b74dc6a04f2f105ff384eec92bc028a9eb4
Richard Bray <richardbray89@gmail.com>
2021-07-14 10:23:26 +0100

    fix(EIA-89): missing infectedFIles array from app init

--- api/controllers/ApplicationTypeController.js
+++ api/controllers/ApplicationTypeController.js
@@ -25,6 +25,7 @@ module.exports = {
           uploadedFileData: [],
           uploadMessages: {
             errors: [],
+            infectedFiles: [],
             fileCountError: false
           }
         };
