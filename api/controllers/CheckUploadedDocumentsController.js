b575dec4432d950e1acaa04b6178fb98c53f6529
Richard Bray <richardbray89@gmail.com>
2021-07-19 18:04:07 +0100

    feat: add eApp specfic payment logic and redirect

--- api/controllers/CheckUploadedDocumentsController.js
+++ api/controllers/CheckUploadedDocumentsController.js
@@ -1,4 +1,5 @@
 const inDevEnvironment = process.env.NODE_ENV === 'development';
+const COST_PER_DOCUMENT = 30;
 
 const CheckUploadedDocumentsController = {
     renderPage(req, res) {
@@ -25,7 +26,8 @@ const CheckUploadedDocumentsController = {
                     `Url for document ${uploadedFile.filename} added to db`
                 );
             });
-            res.redirect('/check-uploaded-documents');
+            // Redirect handled here
+            CheckUploadedDocumentsController._checkPaymentDetailsExistsInDB(req, res);
         } catch (err) {
             sails.log.error(err);
             res.serverError();
@@ -42,6 +44,66 @@ const CheckUploadedDocumentsController = {
             application_id: sessionData.appId || 0, // TODO throw error if this value is false
             uploaded_url: fileUrl,
         };
+    },
+
+    _checkPaymentDetailsExistsInDB(req, res) {
+        const { appId, eApp, payment_reference: paymentRef } = req.session;
+        const totalPrice = eApp.uploadedFileData.length * COST_PER_DOCUMENT;
+        const redirectUrl = sails.config.payment.paymentStartPageUrl;
+        const paymentParams = { appId, totalPrice, paymentRef, redirectUrl };
+
+        ApplicationPaymentDetails.find({
+            where: {
+                application_id: appId,
+            },
+        }).then((data) => {
+            if (!data) {
+                CheckUploadedDocumentsController._addPaymentDetailsToDB(
+                    paymentParams, res
+                );
+            } else {
+                CheckUploadedDocumentsController._updatePaymentAmountInDB(
+                    paymentParams, res
+                );
+            }
+        });
+    },
+
+    _addPaymentDetailsToDB(paymentParams, res) {
+        ApplicationPaymentDetails.create({
+            application_id: paymentParams.appId,
+            payment_amount: paymentParams.totalPrice,
+            oneclick_reference: paymentParams.paymentRef,
+        })
+            .then(() => {
+                sails.log.info(
+                    `Payment details added to db for application ${paymentParams.appId}`
+                );
+                res.redirect(307, paymentParams.redirectUrl);
+            })
+            .catch((error) => {
+                sails.log.error(error);
+                res.serverError();
+            });
+    },
+
+    _updatePaymentAmountInDB(paymentParams, res) {
+        ApplicationPaymentDetails.update(
+            {
+                payment_amount: paymentParams.totalPrice,
+            },
+            { where: { application_id: paymentParams.appId } }
+        )
+            .then(() => {
+                sails.log.info(
+                    `Payment details updated to db for application ${paymentParams.appId}`
+                );
+                res.redirect(307, paymentParams.redirectUrl);
+            })
+            .catch((error) => {
+                sails.log.error(error);
+                res.serverError();
+            });
     },
 };
 
