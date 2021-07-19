c47e72d308982ce00bee43301c6094e886c71813
Richard Bray <richardbray89@gmail.com>
2021-07-19 11:57:19 +0100

    refactor(EIA-89): add prettier rc and format files

--- api/controllers/CheckUploadedDocumentsController.js
+++ api/controllers/CheckUploadedDocumentsController.js
@@ -1,43 +1,48 @@
-const inDevEnvironment = process.env.NODE_ENV === "development";
+const inDevEnvironment = process.env.NODE_ENV === 'development';
 
 const CheckUploadedDocumentsController = {
-  renderPage(req, res) {
-    const userData = HelperService.getUserData(req, res);
-    return res.view("eApostilles/checkUploadedDocuments.ejs", {
-      user_data: userData,
-    });
-  },
+    renderPage(req, res) {
+        const userData = HelperService.getUserData(req, res);
+        return res.view('eApostilles/checkUploadedDocuments.ejs', {
+            user_data: userData,
+        });
+    },
 
-  addDocsToDBHandler(req, res) {
-    try {
-      const { uploadedFileData } = req.session.eApp;
-      if (uploadedFileData.length === 0) {
-        throw new Error("No files uploaded");
-      }
-      uploadedFileData.forEach(async (uploadedFile) => {
-        await UploadedDocumentUrls.create(
-          CheckUploadedDocumentsController._dbColumnData(uploadedFile, req)
-        );
-        sails.log.info(`Url for document ${uploadedFile.filename} added to db`);
-      });
-      res.redirect("/check-uploaded-documents");
-    } catch (err) {
-      sails.log.error(err);
-      res.serverError();
-    }
-  },
+    addDocsToDBHandler(req, res) {
+        try {
+            const { uploadedFileData } = req.session.eApp;
+            if (uploadedFileData.length === 0) {
+                throw new Error('No files uploaded');
+            }
+            uploadedFileData.forEach(async (uploadedFile) => {
+                await UploadedDocumentUrls.create(
+                    CheckUploadedDocumentsController._dbColumnData(
+                        uploadedFile,
+                        req
+                    )
+                );
+                sails.log.info(
+                    `Url for document ${uploadedFile.filename} added to db`
+                );
+            });
+            res.redirect('/check-uploaded-documents');
+        } catch (err) {
+            sails.log.error(err);
+            res.serverError();
+        }
+    },
 
-  _dbColumnData(uploadedFile, req) {
-    const sessionData = req.session;
-    const fileUrl = inDevEnvironment
-      ? uploadedFile.storageName
-      : uploadedFile.location;
+    _dbColumnData(uploadedFile, req) {
+        const sessionData = req.session;
+        const fileUrl = inDevEnvironment
+            ? uploadedFile.storageName
+            : uploadedFile.location;
 
-    return {
-      application_id: sessionData.appId || 0, // TODO throw error if this value is false
-      uploaded_url: fileUrl,
-    };
-  },
+        return {
+            application_id: sessionData.appId || 0, // TODO throw error if this value is false
+            uploaded_url: fileUrl,
+        };
+    },
 };
 
 module.exports = CheckUploadedDocumentsController;
--- api/controllers/FileUploadController.js
+++ api/controllers/FileUploadController.js
@@ -1,117 +1,119 @@
-const multer = require("multer");
-const sails = require("sails");
+const multer = require('multer');
+const sails = require('sails');
 
-const uploadFileToStorage = require("../helpers/uploadFileToStorage");
-const deleteFileFromStorage = require("../helpers/deleteFileFromStorage");
+const uploadFileToStorage = require('../helpers/uploadFileToStorage');
+const deleteFileFromStorage = require('../helpers/deleteFileFromStorage');
 const {
-  virusScanFile,
-  checkTypeSizeAndDuplication,
-} = require("../helpers/uploadedFileErrorChecks");
+    virusScanFile,
+    checkTypeSizeAndDuplication,
+} = require('../helpers/uploadedFileErrorChecks');
 
 const MAX_FILES = 20;
-const FORM_INPUT_NAME = "documents";
-const MULTER_FILE_COUNT_ERR_CODE = "LIMIT_FILE_COUNT";
-const inDevEnvironment = process.env.NODE_ENV === "development";
+const FORM_INPUT_NAME = 'documents';
+const MULTER_FILE_COUNT_ERR_CODE = 'LIMIT_FILE_COUNT';
+const inDevEnvironment = process.env.NODE_ENV === 'development';
 
 const FileUploadController = {
-  uploadFilesPage(req, res) {
-    const userData = HelperService.getUserData(req, res);
-    if (!userData.loggedIn) {
-      sails.log.error("User is not logged in:", userData);
-      return res.forbidden();
-    }
-    return res.view("eApostilles/uploadFiles.ejs", {
-      user_data: userData,
-    });
-  },
+    uploadFilesPage(req, res) {
+        const userData = HelperService.getUserData(req, res);
+        if (!userData.loggedIn) {
+            sails.log.error('User is not logged in:', userData);
+            return res.forbidden();
+        }
+        return res.view('eApostilles/uploadFiles.ejs', {
+            user_data: userData,
+        });
+    },
 
-  uploadFileHandler(req, res) {
-    FileUploadController._clearExistingErrorMessages(req);
-    const uploadFileWithMulter = FileUploadController._multerSetup(req);
-    uploadFileWithMulter(req, res, (err) =>
-      FileUploadController._checkFilesForErrors(req, res, err)
-    );
-  },
+    uploadFileHandler(req, res) {
+        FileUploadController._clearExistingErrorMessages(req);
+        const uploadFileWithMulter = FileUploadController._multerSetup(req);
+        uploadFileWithMulter(req, res, (err) =>
+            FileUploadController._checkFilesForErrors(req, res, err)
+        );
+    },
 
-  _multerSetup(req) {
-    const { s3_bucket: s3BucketName } = req._sails.config.eAppS3Vals;
-    const multerOptions = {
-      storage: uploadFileToStorage(s3BucketName),
-      fileFilter: checkTypeSizeAndDuplication,
-      limits: {
-        files: MAX_FILES,
-      },
-    };
+    _multerSetup(req) {
+        const { s3_bucket: s3BucketName } = req._sails.config.eAppS3Vals;
+        const multerOptions = {
+            storage: uploadFileToStorage(s3BucketName),
+            fileFilter: checkTypeSizeAndDuplication,
+            limits: {
+                files: MAX_FILES,
+            },
+        };
 
-    return multer(multerOptions).array(FORM_INPUT_NAME);
-  },
+        return multer(multerOptions).array(FORM_INPUT_NAME);
+    },
 
-  _clearExistingErrorMessages(req) {
-    req.session.eApp.uploadMessages.errors = [];
-    req.session.eApp.uploadMessages.fileCountError = false;
-    req.session.eApp.uploadMessages.infectedFiles = [];
-  },
+    _clearExistingErrorMessages(req) {
+        req.session.eApp.uploadMessages.errors = [];
+        req.session.eApp.uploadMessages.fileCountError = false;
+        req.session.eApp.uploadMessages.infectedFiles = [];
+    },
 
-  _checkFilesForErrors(req, res, err) {
-    const { clamav_enabled: clamavEnabled } = req._sails.config.eAppS3Vals;
-    if (err) {
-      const fileLimitExceeded = err.code === MULTER_FILE_COUNT_ERR_CODE;
+    _checkFilesForErrors(req, res, err) {
+        const { clamav_enabled: clamavEnabled } = req._sails.config.eAppS3Vals;
+        if (err) {
+            const fileLimitExceeded = err.code === MULTER_FILE_COUNT_ERR_CODE;
 
-      if (fileLimitExceeded) {
-        req.session.eApp.uploadMessages.fileCountError = true;
-        sails.log.error(err.message, err.stack);
-      } else {
-        sails.log.error(err);
-        res.serverError(err);
-      }
-    } else {
-      clamavEnabled && virusScanFile(req);
-      !inDevEnvironment && FileUploadController._addS3LocationToSession(req);
-    }
+            if (fileLimitExceeded) {
+                req.session.eApp.uploadMessages.fileCountError = true;
+                sails.log.error(err.message, err.stack);
+            } else {
+                sails.log.error(err);
+                res.serverError(err);
+            }
+        } else {
+            clamavEnabled && virusScanFile(req);
+            !inDevEnvironment &&
+                FileUploadController._addS3LocationToSession(req);
+        }
 
-    FileUploadController._redirectToUploadPage(res);
-  },
+        FileUploadController._redirectToUploadPage(res);
+    },
 
-  _addS3LocationToSession(req) {
-    const { uploadedFileData } = req.session.eApp;
-    uploadedFileData.forEach((uploadedFile, index) => {
-      uploadedFile.location = req.files[index].location;
-    });
-    req.session.eApp.uploadedFileData = uploadedFileData;
-  },
+    _addS3LocationToSession(req) {
+        const { uploadedFileData } = req.session.eApp;
+        uploadedFileData.forEach((uploadedFile, index) => {
+            uploadedFile.location = req.files[index].location;
+        });
+        req.session.eApp.uploadedFileData = uploadedFileData;
+    },
 
-  deleteFileHandler(req, res) {
-    const { uploadedFileData } = req.session.eApp;
-    if (!req.body.delete) {
-      sails.log.error("Item to delete wasn't specified");
-      return res.badRequest();
-    }
-    if (uploadedFileData.length === 0) {
-      sails.log.info("Item to delete wasn't found");
-      return res.notFound();
-    }
-    const updatedSession = FileUploadController._removeFileFromSessionArray(
-      req,
-      uploadedFileData
-    );
-    req.session.eApp.uploadedFileData = updatedSession;
-    FileUploadController._redirectToUploadPage(res);
-  },
+    deleteFileHandler(req, res) {
+        const { uploadedFileData } = req.session.eApp;
+        if (!req.body.delete) {
+            sails.log.error("Item to delete wasn't specified");
+            return res.badRequest();
+        }
+        if (uploadedFileData.length === 0) {
+            sails.log.info("Item to delete wasn't found");
+            return res.notFound();
+        }
+        const updatedSession = FileUploadController._removeFileFromSessionArray(
+            req,
+            uploadedFileData
+        );
+        req.session.eApp.uploadedFileData = updatedSession;
+        FileUploadController._redirectToUploadPage(res);
+    },
 
-  _removeFileFromSessionArray(req, uploadedFileData) {
-    const { s3_bucket: s3BucketName } = req._sails.config.eAppS3Vals;
-    return uploadedFileData.filter((uploadedFile) => {
-      const fileToDeleteExists = uploadedFile.filename === req.body.delete;
-      if (fileToDeleteExists) {
-        deleteFileFromStorage(uploadedFile, s3BucketName);
-      }
-      return uploadedFile.filename !== req.body.delete;
-    });
-  },
+    _removeFileFromSessionArray(req, uploadedFileData) {
+        const { s3_bucket: s3BucketName } = req._sails.config.eAppS3Vals;
+        return uploadedFileData.filter((uploadedFile) => {
+            const fileToDeleteExists =
+                uploadedFile.filename === req.body.delete;
+            if (fileToDeleteExists) {
+                deleteFileFromStorage(uploadedFile, s3BucketName);
+            }
+            return uploadedFile.filename !== req.body.delete;
+        });
+    },
 
-  _redirectToUploadPage(res) {
-    res.redirect("/upload-files");
-  },
+    _redirectToUploadPage(res) {
+        res.redirect('/upload-files');
+    },
 };
 
 module.exports = FileUploadController;
