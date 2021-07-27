f2106fb9209be8570bd8fe2b57553b269b2a8036
Richard Bray <richardbray89@gmail.com>
2021-07-27 10:10:31 +0100

    chore: upadte pm2 config for payment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -25,41 +25,41 @@
 // Where <id> is the id of the service (run `pm2 ls` to list all services).
 
 module.exports = {
-  apps: [
-    {
-      name: "loi-application-service",
-      script: "npm",
-      args: "run start"
-    },
-    {
-      name: "loi-user-service",
-      script: "npm",
-      args: "run start",
-      cwd: "../loi-user-service"
-    },
-    {
-      name: "loi-submission-service",
-      script: "npm",
-      args: "run start",
-      cwd: "../loi-submission-service"
-    },
-    {
-      name: "loi-notification-service",
-      script: "node",
-      args: "server.js 1234",
-      cwd: "../loi-notification-service"
-    },
-    {
-      name: "loi-address-service",
-      script: "node",
-      args: "server.js 7878",
-      cwd: "../loi-address-service"
-    },
-    {
-      name: "loi-payment-service",
-      script: "node",
-      args: "server.js 4321",
-      cwd: "../loi-payment-service"
-    }
-  ]
+    apps: [
+        {
+            name: 'loi-application-service',
+            script: 'npm',
+            args: 'run start',
+        },
+        {
+            name: 'loi-user-service',
+            script: 'npm',
+            args: 'run start',
+            cwd: '../loi-user-service',
+        },
+        {
+            name: 'loi-submission-service',
+            script: 'npm',
+            args: 'run start',
+            cwd: '../loi-submission-service',
+        },
+        {
+            name: 'loi-notification-service',
+            script: 'node',
+            args: 'server.js 1234',
+            cwd: '../loi-notification-service',
+        },
+        {
+            name: 'loi-address-service',
+            script: 'node',
+            args: 'server.js 7878',
+            cwd: '../loi-address-service',
+        },
+        {
+            name: 'loi-payment-service',
+            script: 'npm',
+            args: 'run start',
+            cwd: '../loi-payment-service',
+        },
+    ],
 };
