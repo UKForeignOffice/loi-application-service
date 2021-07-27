2663ad88cd929bda369d99e3a7f2ad23821acb5b
Richard Bray <richardbray89@gmail.com>
2021-07-27 18:04:25 +0100

    chore: migration to create eApp export table

---
+++ database_change_log/EIA-128_create_ExportateEAppData_table.sql
@@ -0,0 +1,27 @@
+-- Run against FCO-LOI-Service fco_service
+
+CREATE TABLE public."ExportedEAppData"
+(
+    id integer DEFAULT nextval('exported_data'::regclass),
+    application_id integer,
+    "applicationType" text COLLATE pg_catalog."default",
+    first_name character varying(255) COLLATE pg_catalog."default",
+    last_name character varying(255) COLLATE pg_catalog."default",
+    telephone character varying(25) COLLATE pg_catalog."default",
+    email character varying(255) COLLATE pg_catalog."default",
+    doc_count integer,
+    user_ref text COLLATE pg_catalog."default",
+    payment_reference text COLLATE pg_catalog."default",
+    payment_amount numeric,
+    feedback_consent boolean,
+    unique_app_id text COLLATE pg_catalog."default",
+    user_id integer,
+    company_name text COLLATE pg_catalog."default",
+    "createdAt" date,
+    "updatedAt" date
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public."ExportedEAppData"
+    OWNER to postgres;
