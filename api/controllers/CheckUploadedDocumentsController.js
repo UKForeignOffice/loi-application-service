f1f2ba9b1045d8ada0aa64fa7083cf7d247b93a6
Richard Bray <richardbray89@gmail.com>
2021-07-28 19:16:26 +0100

    chore: run payment details after document count

--- api/controllers/CheckUploadedDocumentsController.js
+++ api/controllers/CheckUploadedDocumentsController.js
@@ -66,21 +66,16 @@ const CheckUploadedDocumentsController = {
         const documentCount = eApp.uploadedFileData.length;
         const totalPrice = documentCount * COST_PER_DOCUMENT;
         const redirectUrl = req._sails.config.payment.paymentStartPageUrl;
-        const paymentParams = { appId, totalPrice, paymentRef, redirectUrl };
-        const documentCountParams = {
+        const params = {
             appId,
             totalPrice,
+            paymentRef,
             documentCount,
+            redirectUrl,
         };
-        CheckUploadedDocumentsController._checkDocumentCountInDB(
-            documentCountParams,
-            res
-        );
-        // Redirect to payment handled here
-        CheckUploadedDocumentsController._checkPaymentDetailsExistsInDB(
-            paymentParams,
-            res
-        );
+
+        CheckUploadedDocumentsController._checkDocumentCountInDB(params, res);
+
         // Here for testing purposes, will move to it's own controller later
         CheckUploadedDocumentsController._checkAdditionalApplicationInfoInDB(
             appId,
@@ -88,35 +83,39 @@ const CheckUploadedDocumentsController = {
         );
     },
 
-    _checkDocumentCountInDB(documentCountParams, res) {
+    _checkDocumentCountInDB(params, res) {
         UserDocumentCount.find({
             where: {
-                application_id: documentCountParams.appId,
+                application_id: params.appId,
             },
         }).then((data) => {
             if (!data) {
                 CheckUploadedDocumentsController._createDocumentCountInDB(
-                    documentCountParams,
+                    params,
                     res
                 );
             } else {
                 CheckUploadedDocumentsController._updateDocumentCountInDB(
-                    documentCountParams,
+                    params,
                     res
                 );
             }
         });
     },
 
-    _createDocumentCountInDB(documentCountParams, res) {
+    _createDocumentCountInDB(params, res) {
         UserDocumentCount.create({
-            application_id: documentCountParams.appId,
-            doc_count: documentCountParams.documentCount,
-            price: documentCountParams.totalPrice,
+            application_id: params.appId,
+            doc_count: params.documentCount,
+            price: params.totalPrice,
         })
             .then(() => {
                 sails.log.info(
-                    `Document count added to db for appId ${documentCountParams.appId}`
+                    `Document count added to db for appId ${params.appId}`
+                );
+                CheckUploadedDocumentsController._checkPaymentDetailsExistsInDB(
+                    params,
+                    res
                 );
             })
             .catch((err) => {
@@ -125,17 +124,21 @@ const CheckUploadedDocumentsController = {
             });
     },
 
-    _updateDocumentCountInDB(documentCountParams, res) {
+    _updateDocumentCountInDB(params, res) {
         UserDocumentCount.update(
             {
-                doc_count: documentCountParams.documentCount,
-                price: documentCountParams.totalPrice,
+                doc_count: params.documentCount,
+                price: params.totalPrice,
             },
-            { where: { application_id: documentCountParams.appId } }
+            { where: { application_id: params.appId } }
         )
             .then(() => {
                 sails.log.info(
-                    `Document count updated in db for appId ${documentCountParams.appId}`
+                    `Document count updated in db for appId ${params.appId}`
+                );
+                CheckUploadedDocumentsController._checkPaymentDetailsExistsInDB(
+                    params,
+                    res
                 );
             })
             .catch((err) => {
@@ -144,37 +147,37 @@ const CheckUploadedDocumentsController = {
             });
     },
 
-    _checkPaymentDetailsExistsInDB(paymentParams, res) {
+    _checkPaymentDetailsExistsInDB(params, res) {
         ApplicationPaymentDetails.find({
             where: {
-                application_id: paymentParams.appId,
+                application_id: params.appId,
             },
         }).then((data) => {
             if (!data) {
                 CheckUploadedDocumentsController._createPaymentDetailsInDB(
-                    paymentParams,
+                    params,
                     res
                 );
             } else {
                 CheckUploadedDocumentsController._updatePaymentAmountInDB(
-                    paymentParams,
+                    params,
                     res
                 );
             }
         });
     },
 
-    _createPaymentDetailsInDB(paymentParams, res) {
+    _createPaymentDetailsInDB(params, res) {
         ApplicationPaymentDetails.create({
-            application_id: paymentParams.appId,
-            payment_amount: paymentParams.totalPrice,
-            oneclick_reference: paymentParams.paymentRef,
+            application_id: params.appId,
+            payment_amount: params.totalPrice,
+            oneclick_reference: params.paymentRef,
         })
             .then(() => {
                 sails.log.info(
-                    `Payment details added to db for application ${paymentParams.appId}`
+                    `Payment details added to db for application ${params.appId}`
                 );
-                res.redirect(307, paymentParams.redirectUrl);
+                res.redirect(307, params.redirectUrl);
             })
             .catch((error) => {
                 sails.log.error(error);
@@ -182,18 +185,18 @@ const CheckUploadedDocumentsController = {
             });
     },
 
-    _updatePaymentAmountInDB(paymentParams, res) {
+    _updatePaymentAmountInDB(params, res) {
         ApplicationPaymentDetails.update(
             {
-                payment_amount: paymentParams.totalPrice,
+                payment_amount: params.totalPrice,
             },
-            { where: { application_id: paymentParams.appId } }
+            { where: { application_id: params.appId } }
         )
             .then(() => {
                 sails.log.info(
-                    `Payment details updated in db for application ${paymentParams.appId}`
+                    `Payment details updated in db for application ${params.appId}`
                 );
-                res.redirect(307, paymentParams.redirectUrl);
+                res.redirect(307, params.redirectUrl);
             })
             .catch((error) => {
                 sails.log.error(error);
