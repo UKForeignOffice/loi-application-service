a22ac5fb2e265d31a6df0f6aee984a58023e533a
Richard Bray <richardbray89@gmail.com>
2021-08-06 18:35:17 +0100

    tests: add tests for eApp dashboard page

--- tests/specs/controllers/Dashboard.test.js
+++ tests/specs/controllers/Dashboard.test.js
@@ -8,35 +8,171 @@
 var request = require('supertest');
 var chai = require('chai');
 
-describe('DashboardController:', function() {
+const { expect } = require('chai');
+const sinon = require('sinon');
+const dashboardController = require('../../../api/controllers/DashboardController');
 
-/* FUNCTION: dashboard ---------------------------------------------------------
- *
- */
-    describe('[Function: dashboard]', function() {
+describe('DashboardController:', function () {
+    /* FUNCTION: dashboard ---------------------------------------------------------
+     *
+     */
+    describe('[Function: dashboard]', function () {
         it('should load dashboard displaying a list of previously submitted applications for the current user, if any exist', function (done) {
-
-            var userApplicationsSql = 'select app."createdAt" as "createdDate", at."applicationType" as "appType", udc."doc_count" as "documentCount", app.application_reference as "appReference", \'Pending\' as "paymentStatus", \'Online application in progress\' as status from "Application" app inner join "ApplicationTypes" at on app."serviceType" = at.id';
-            userApplicationsSql += ' inner join "UserDocumentCount" udc on app.application_id=udc.application_id ';
+            var userApplicationsSql =
+                'select app."createdAt" as "createdDate", at."applicationType" as "appType", udc."doc_count" as "documentCount", app.application_reference as "appReference", \'Pending\' as "paymentStatus", \'Online application in progress\' as status from "Application" app inner join "ApplicationTypes" at on app."serviceType" = at.id';
+            userApplicationsSql +=
+                ' inner join "UserDocumentCount" udc on app.application_id=udc.application_id ';
             userApplicationsSql += ' limit 0 '; // get first result to remove need for the 'where app_id=' clause
 
-            sequelize.query(userApplicationsSql)
+            sequelize
+                .query(userApplicationsSql)
                 .spread(function (results, metadata) {
-                    chai.assert.isOk('Previous applications submitted search successful');
+                    chai.assert.isOk(
+                        'Previous applications submitted search successful'
+                    );
 
                     if (results) {
-                        chai.assert.isOk('Previous applications submitted by user found');
+                        chai.assert.isOk(
+                            'Previous applications submitted by user found'
+                        );
                     }
-                        done();
-                })
-            .catch(Sequelize.ValidationError, function(error) {
-                    chai.assert.isNotOk('There was a problem getting all previous applications from the db ', error);
                     done();
-            });
-
+                })
+                .catch(Sequelize.ValidationError, function (error) {
+                    chai.assert.isNotOk(
+                        'There was a problem getting all previous applications from the db ',
+                        error
+                    );
+                    done();
+                });
         });
     });
 
+    describe('tests for users flagged to use the eAposttie service', () => {
+        let reqStub;
+        let resStub;
+        const sandbox = sinon.sandbox.create();
 
+        function assertWhenPromisesResolved(assertion) {
+            setTimeout(assertion);
+        }
 
+        beforeEach(() => {
+            reqStub = {
+                session: {
+                    passport: {
+                        user: 189,
+                    },
+                    user: {
+                        id: 128,
+                    },
+                    appId: 22,
+                },
+                _sails: {
+                    config: {
+                        customURLs: {
+                            userServiceURL: '',
+                            applicationStatusAPIURL: '',
+                        },
+                        casebookCertificate: '',
+                        casebookKey: '',
+                        hmacKey: '',
+                    },
+                },
+                params: {
+                    unique_app_id: '123',
+                },
+                allParams: sandbox.stub().returns({
+                    dashboardFilter: '',
+                }),
+                flash: sandbox.spy(),
+                query: {
+                    searchText: '',
+                    sortOrder: 1,
+                    page: 1,
+                    ajax: false,
+                },
+            };
+
+            resStub = {
+                view: sandbox.spy(),
+                redirect: sandbox.spy(),
+            };
+
+            sandbox.spy(sails.log, 'error');
+            sandbox.spy(sails.log, 'info');
+
+        });
+
+        afterEach(() => {
+            sandbox.restore();
+        });
+
+        it('should process electronic apps if user.electronicEnabled is true', () => {
+            // when
+            sandbox.stub(Application, 'count').resolves(6);
+            sandbox.stub(HelperService, 'refreshUserData').resolves();
+            sandbox.stub(HelperService, 'getUserData').callsFake(() => ({
+                user: {
+                    electronicEnabled: true,
+                },
+            }));
+            dashboardController.dashboard(reqStub, resStub);
+
+            // then
+            assertWhenPromisesResolved(
+                () =>
+                    expect(
+                        sails.log.info.calledWith(
+                            'Fetching electronic and paper applications'
+                        )
+                    ).to.be.true
+            );
+        });
+
+        it('should run the eApp specific stored procedure if user.electronicEnabled is true', () => {
+            // when
+            const electronicEnabled = true;
+            const chooseStoredProcedure =
+                dashboardController._chooseStoredProcedure(
+                    null,
+                    electronicEnabled
+                );
+
+            // then
+            const expectedQueryValue =
+                'SELECT * FROM dashboard_data_eapp(:userId, :pageSize, :offset, :sortOrder, :direction, :queryString)';
+            expect(chooseStoredProcedure).to.equal(expectedQueryValue);
+        });
+
+        it('should display the updated dashboard page', () => {
+            // when
+            const stubPageAttributes = {
+                message: '',
+                users_applications: [],
+                moment: {},
+                offset: 1,
+                sortOrder: 1,
+                paginationMessage: '',
+                currentPage: 1,
+                totalPages: 1,
+                searchCriteria: '',
+                user_data: {
+                    user: {
+                        electronicEnabled: true,
+                    },
+                },
+                application_total: 0,
+            };
+            dashboardController._redirectToPage(
+                stubPageAttributes,
+                reqStub,
+                resStub
+            );
+
+            // then
+            const expectedPageUrl = 'eApostilles/dashboard.ejs';
+            expect(resStub.view.calledWith(expectedPageUrl)).to.be.true;
+        });
+    });
 });
