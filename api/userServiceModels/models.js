6219a3e8840e62a786a28470aa37b8bd701d9d35
Richard Bray <richardbray89@gmail.com>
2021-08-12 11:43:46 +0100

    test: add more tests for sessionExpired

--- api/userServiceModels/models.js
+++ api/userServiceModels/models.js
@@ -1,17 +1,29 @@
-var UserMeta = require('./User.js'),
-    AccountDetailsMeta = require('./AccountDetails.js'),
-    SavedAddressMeta = require('./SavedAddress.js'),
-    usersDbConn = sails.config.userServiceSequelize;
-
-var User = usersDbConn.define('Users', UserMeta.attributes, UserMeta.options);
-var AccountDetails = usersDbConn.define('AccountDetails', AccountDetailsMeta.attributes, AccountDetailsMeta.options);
-var SavedAddress = usersDbConn.define('SavedAddress', SavedAddressMeta.attributes, SavedAddressMeta.options);
+const UserMeta = require('./User.js');
+const AccountDetailsMeta = require('./AccountDetails.js');
+const SavedAddressMeta = require('./SavedAddress.js');
 
 // you can define relationships here
 
-module.exports.User = User;
-module.exports.AccountDetails = AccountDetails;
-module.exports.SavedAddress = SavedAddress;
+module.exports = (usersDbConn) => {
+    const User = usersDbConn.define(
+        'Users',
+        UserMeta.attributes,
+        UserMeta.options
+    );
+    const AccountDetails = usersDbConn.define(
+        'AccountDetails',
+        AccountDetailsMeta.attributes,
+        AccountDetailsMeta.options
+    );
+    const SavedAddress = usersDbConn.define(
+        'SavedAddress',
+        SavedAddressMeta.attributes,
+        SavedAddressMeta.options
+    );
 
-
-
+    return {
+        User,
+        AccountDetails,
+        SavedAddress,
+    };
+};
