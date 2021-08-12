2eadce9dac7b6eac578ac698c640230989b93e35
Richard Bray <richardbray89@gmail.com>
2021-08-12 14:24:44 +0100

    test: add coverage report for Jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  testEnvironment: 'jsdom',
-  testMatch: ['**/tests/ui/**/*.test.js'],
-  setupFiles: ['./tests/jest.setup.js'],
-  setupFilesAfterEnv: ['jest-extended', './tests/jest.setup-after-env.js']
-}
+    testEnvironment: 'jsdom',
+    testMatch: ['**/tests/ui/**/*.test.js'],
+    setupFiles: ['./tests/jest.setup.js'],
+    setupFilesAfterEnv: ['jest-extended', './tests/jest.setup-after-env.js'],
+    collectCoverage: true,
+    collectCoverageFrom: ['**/tests/ui/**/*.test.js', '!**/assets/js/**'],
+    coverageReporters: ['json', 'html'],
+};
