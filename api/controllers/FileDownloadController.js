287cb7c22b1ccdb31d623bcd92486ef4b741fef3
Richard Bray <richardbray89@gmail.com>
2021-08-17 18:48:13 +0100

    chore: start work on download file endpoint

---
+++ api/controllers/FileDownloadController.js
@@ -0,0 +1,6 @@
+const FileDownloadController = {
+    downloadFileHandler(req, res) {
+        // make api call to casebookR
+        // .../downløadApostille?timestampe=[time]&apostilleReferenece=[value<get from query params>] GET
+    }
+}
