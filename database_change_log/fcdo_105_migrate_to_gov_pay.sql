6d1efac3887a6198cb202c655ff0431ee567d628
Simon Moore <s.moore@kainos.com>
2021-09-14 15:05:46 +0100

    Update to sql

--- database_change_log/fcdo_105_migrate_to_gov_pay.sql
+++ database_change_log/fcdo_105_migrate_to_gov_pay.sql
@@ -16,13 +16,16 @@ INSERT INTO public."PaymentsCleanupJob" (id, lock) VALUES (1, false);
 
 CREATE TABLE public."AdditionalPaymentDetails"
 (
-    application_id integer NOT NULL,
+    application_id text COLLATE pg_catalog."default" NOT NULL,
     payment_reference text COLLATE pg_catalog."default",
     payment_amount numeric(10,2) NOT NULL DEFAULT 0.00,
     payment_status text COLLATE pg_catalog."default",
     payment_complete boolean NOT NULL DEFAULT false,
     created_at timestamp with time zone DEFAULT ('now'::text)::timestamp with time zone,
     updated_at timestamp with time zone DEFAULT ('now'::text)::timestamp with time zone,
+    submitted text COLLATE pg_catalog."default",
+    submission_attempts integer DEFAULT 0,
+    casebook_response_code text COLLATE pg_catalog."default",
     CONSTRAINT "application_id_pk" PRIMARY KEY (application_id)
 )
     WITH (
