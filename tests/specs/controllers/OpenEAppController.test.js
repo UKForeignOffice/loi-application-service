40bf0d0575193caa7d688b9c59ab191a13102ae4
Richard Bray <richardbray89@gmail.com>
2021-10-06 16:33:41 +0100

    chore: attempting to fix first skipped test

--- tests/specs/controllers/OpenEAppController.test.js
+++ tests/specs/controllers/OpenEAppController.test.js
@@ -139,7 +139,7 @@ describe('OpenEAppController', () => {
             );
         });
 
-        it('should render openEApp.ejs page with correct data', () => {
+        it.only('should render openEApp.ejs page with correct data', () => {
             // when - beforeEach runs
             // then
             assertWhenPromisesResolved(() =>
