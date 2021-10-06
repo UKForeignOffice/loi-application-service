ecb98b517d2f884da78d94c66339fd35c81278d0
Richard Bray <richardbray89@gmail.com>
2021-10-06 15:23:19 +0100

    refactor: remove need to fetch domain from .env

--- api/controllers/EAppSubmittedController.js
+++ api/controllers/EAppSubmittedController.js
@@ -82,11 +82,9 @@ const EAppSubmittedController = {
     _sendConfirmationEmail(userDetails, applicationId, req) {
         const emailAddress = userDetails.email;
         const applicationRef = applicationId;
-        const { domain } = req._sails.config.session;
         const sendInformation = {
             first_name: userDetails.firstName,
             last_name: userDetails.lastName,
-            app_url: `${domain}open-eapp/${applicationRef}`,
         };
         const userRef = userDetails.userRef;
         const serviceType = userDetails.appType;
