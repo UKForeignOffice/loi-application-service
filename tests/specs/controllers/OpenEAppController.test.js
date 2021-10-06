8d0906b49c0033d8bcdfbf4322c3720fea1cb685
Richard Bray <richardbray89@gmail.com>
2021-10-06 16:26:39 +0100

    chore: fix skilled tests 1/3

--- tests/specs/controllers/OpenEAppController.test.js
+++ tests/specs/controllers/OpenEAppController.test.js
@@ -139,7 +139,7 @@ describe('OpenEAppController', () => {
             );
         });
 
-        it.skip('should render openEApp.ejs page with correct data', () => {
+        it('should render openEApp.ejs page with correct data', () => {
             // when - beforeEach runs
             // then
             assertWhenPromisesResolved(() =>
