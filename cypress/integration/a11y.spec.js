fad3f9121580e616ea01d5ab898ed08e255c24ed
Richard Bray <richardbray89@gmail.com>
2021-10-29 18:29:40 +0100

    test: add testing-library to cypress

--- cypress/integration/a11y.spec.js
+++ cypress/integration/a11y.spec.js
@@ -28,49 +28,47 @@ describe('Check accessiblity', () => {
         beforeEach(() => {
             cy.visit('/');
         });
+
         it('Get your document legalised', () => {
             checkA11y();
         });
 
         it('Choose a service', () => {
-            cy.get('.govuk-button--start').contains('Start now').click();
+            cy.findByRole('button', { name: 'Start now' }).click();
             checkA11y();
         });
     });
 
     describe('Post login', () => {
         function uploadTestFile() {
-            cy.get('.multi-file-upload__input').attachFile('test.pdf');
-            cy.get('.govuk-button--secondary').contains('Upload').click();
-            cy.wait(500);
+            cy.findByLabelText('Upload files').attachFile('test.pdf');
+            cy.findByRole('button', { name: 'Upload' }).click();
         }
 
         function clickContinueBtn() {
-            cy.get('.column-two-thirds .govuk-button')
-                .contains('Continue')
-                .click();
+            cy.findByRole('button', { name: 'Continue' }).click();
         }
 
-        function checkFirstRadioAndClickContinue() {
-            cy.get('[type="radio"].govuk-radios__input').first().check();
+        function checkRadioAndClickContinue(radioLabelName) {
+            cy.findByLabelText(radioLabelName).check();
             clickContinueBtn();
         }
 
         function passEappStartScreen() {
-            cy.get('.govuk-body')
-                .contains('skip to the start of the service')
-                .click();
-            cy.get('.govuk-button--start').contains('Start now').click();
+            cy.findByRole('link', {
+                name: 'skip to the start of the service',
+            }).click();
+            clickContinueBtn();
         }
 
         beforeEach(() => {
             cy.visit('/select-service');
-            cy.get('#sign-in-link').contains('Sign in').click();
+            cy.findByRole('link', { name: 'Sign in' }).click();
 
             cy.get('#email').type(Cypress.env('EMAIL'));
             cy.get('#password').type(Cypress.env('PASSWORD'));
 
-            cy.get('#sign-in-button').contains('Sign in').click();
+            cy.findByRole('button', { name: 'Sign in' }).click();
         });
 
         context('eApp eligibility questions', () => {
@@ -84,19 +82,26 @@ describe('Check accessiblity', () => {
             });
 
             it('Is the e-Apostille accepted in the destination country?', () => {
-                checkFirstRadioAndClickContinue();
+                checkRadioAndClickContinue('e-Apostille service');
                 checkA11y();
             });
 
             it('Check if the documents are eligible for the e-Apostille service', () => {
-                checkFirstRadioAndClickContinue();
-                checkFirstRadioAndClickContinue();
+                checkRadioAndClickContinue('e-Apostille service');
+                checkRadioAndClickContinue('Yes');
+                checkA11y();
+            });
+
+            it('You cannot use this service', () => {
+                checkRadioAndClickContinue('e-Apostille service');
+                checkRadioAndClickContinue('No');
                 checkA11y();
             });
 
             it('Have the PDFs been notarised and digitally signed by a notary?', () => {
-                for (var i = 0; i < TOTAL_ELIBIGILITY_QUESTIONS; ++i) {
-                    checkFirstRadioAndClickContinue();
+                checkRadioAndClickContinue('e-Apostille service');
+                for (var i = 0; i < TOTAL_ELIBIGILITY_QUESTIONS - 1; ++i) {
+                    checkRadioAndClickContinue('Yes');
                 }
                 checkA11y();
             });
@@ -104,13 +109,13 @@ describe('Check accessiblity', () => {
 
         context('eApp file upload', () => {
             beforeEach(() => {
-                checkFirstRadioAndClickContinue();
+                checkRadioAndClickContinue('e-Apostille service');
             });
 
             it('Get the documents legalised using the e-Apostille service', () => {
-                cy.get('.govuk-body')
-                    .contains('skip to the start of the service')
-                    .click();
+                cy.findByRole('link', {
+                    name: 'skip to the start of the service',
+                }).click();
                 checkA11y();
             });
 
@@ -119,7 +124,7 @@ describe('Check accessiblity', () => {
                 checkA11y();
             });
 
-            it('Add your PDFs - 1 file uploaded', () => {
+            it.only('Add your PDFs - 1 file uploaded', () => {
                 passEappStartScreen();
                 uploadTestFile();
                 checkA11y();
@@ -134,52 +139,60 @@ describe('Check accessiblity', () => {
         });
 
         context('eApp applications section', () => {
+            function selectFirstApplication() {
+                cy.get('#previousApplications .appRef .govuk-link')
+                    .first()
+                    .click();
+            }
+
             it('Your account', () => {
-                cy.get('#Applications-Link').contains('Applications').click();
+                cy.findByRole('link', {
+                    name: 'Applications',
+                }).click();
                 checkA11y();
             });
 
             it('View app', () => {
-                cy.get('#Applications-Link').contains('Applications').click();
-                cy.get('#previousApplications .appRef .govuk-link')
-                    .first()
-                    .click();
+                cy.findByRole('link', {
+                    name: 'Applications',
+                }).click();
+                selectFirstApplication();
                 checkA11y();
             });
         });
 
         context('eApp summary and success page', () => {
-            function addTestPayDetails() {
+            function confirmTestPayDetails() {
                 cy.get('#card-no').type('4444333322221111');
                 cy.get('#expiry-month').type('12');
                 cy.get('#expiry-year').type('34');
-                cy.get('#cardholder-name').type('T\'Challa Udaku');
-                cy.get('#cvc').type("161");
+                cy.get('#cardholder-name').type("T'Challa Udaku");
+                cy.get('#cvc').type('161');
                 cy.get('#address-line-1').type('Stables Market');
                 cy.get('#address-line-2').type('Chalk Farm Rd');
                 cy.get('#address-city').type('London');
                 cy.get('#address-postcode').type('NW1 8AB');
                 cy.get('#submit-card-details').click();
+                // - Confirm payment page
                 cy.get('#confirm').click();
             }
 
             beforeEach(() => {
-                checkFirstRadioAndClickContinue();
+                checkRadioAndClickContinue('e-Apostille service');
                 passEappStartScreen();
                 uploadTestFile();
+                clickContinueBtn();
                 clickContinueBtn();
             });
 
             it('Summary page', () => {
-                clickContinueBtn();
                 checkA11y();
             });
 
             it('Submission successful', () => {
                 clickContinueBtn();
-                clickContinueBtn();
-                cy.get('.govuk-button').contains('Pay').click();
-                addTestPayDetails();
+                cy.findByRole('button', { name: 'Pay' }).click();
+                confirmTestPayDetails();
                 checkA11y();
             });
         });
