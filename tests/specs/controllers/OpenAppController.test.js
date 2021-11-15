96aacffd04c61ed5a9f19810c9b94c0f1a375dbb
Richard Bray <richardbray89@gmail.com>
2021-11-15 14:41:29 +0000

    test: fix broken tests

---
+++ tests/specs/controllers/OpenAppController.test.js
@@ -0,0 +1,72 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const summaryController = require('../../../api/controllers/SummaryController');
+const OpenAppController = require('../../../api/controllers/OpenAppController');
+
+function assertWhenPromisesResolved(assertion) {
+    setTimeout(assertion);
+}
+
+describe('openCoverSheet', () => {
+    let reqStub;
+    const sandbox = sinon.sandbox.create();
+
+    const resStub = {
+        view: sandbox.spy(),
+    };
+    beforeEach(() => {
+        reqStub = {
+            params: {
+                unique_app_id: 'A-D-21-0920-2180-EEE1',
+            },
+            session: {
+                appId: undefined,
+                user: {
+                    id: 123,
+                },
+            },
+        };
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    it("redirects to 404 page if session and db user ids don't match", () => {
+        // when
+        sandbox.stub(HelperService, 'LoggedInStatus').callsFake(() => true);
+        sandbox.stub(Application, 'find').resolves({
+            user_id: 100,
+            application_id: 124,
+        });
+        OpenAppController.openCoverSheet(reqStub, resStub);
+
+        // then
+        assertWhenPromisesResolved(
+            () => expect(resStub.view.calledWith('404')).to.be.true
+        );
+    });
+
+    it('redirect to 404 page if user is not logged in', () => {
+        // when
+        sandbox.stub(HelperService, 'LoggedInStatus').callsFake(() => false);
+        OpenAppController.openCoverSheet(reqStub, resStub);
+
+        // then
+        expect(resStub.view.calledWith('404')).to.be.true;
+    });
+
+    it('runs fetchAll function if session and db user ids match', () => {
+        // when
+        const fetchAllFn = sandbox.stub(summaryController, 'fetchAll');
+        sandbox.stub(HelperService, 'LoggedInStatus').callsFake(() => true);
+        sandbox.stub(Application, 'find').resolves({
+            user_id: 123,
+            application_id: 124,
+        });
+        OpenAppController.openCoverSheet(reqStub, resStub);
+
+        // then
+        assertWhenPromisesResolved(() => expect(fetchAllFn.called).to.be.true);
+    });
+});
