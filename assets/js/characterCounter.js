6c58bf1e61d2ee19e179bc8bb58abec223045cd3
Richard Bray <richardbray89@gmail.com>
2021-11-17 16:33:23 +0000

    refactor: refactor char counter code

--- assets/js/characterCounter.js
+++ assets/js/characterCounter.js
@@ -1,37 +1,55 @@
-
-const parentElem = document.querySelector(
+const inputParentElem = document.querySelector(
     '[data-module="govuk-character-count"]'
 );
 
-if (parentElem) {
-    const inputElem = parentElem.querySelector('.govuk-js-character-count');
-    const { maxlength } = parentElem.dataset;
-    const inputHint = parentElem.querySelector(
-        '.govuk-character-count__message'
+if (inputParentElem) {
+    prepareCharCounter();
+}
+
+function prepareCharCounter() {
+    const inputElem = inputParentElem.querySelector(
+        '.govuk-js-character-count'
     );
+    const commonParams = {
+        charLimit: Number(inputParentElem.dataset.maxlength),
+        inputElem,
+        inputHint: inputParentElem.querySelector(
+            '.govuk-character-count__message'
+        ),
+    };
 
-    if (inputElem.value.length > 0) {
-        updateRemainingCharacters(maxlength, inputElem, inputHint);
-    } else {
-        inputHint.textContent = `You have ${maxlength} characters remaining`;
-    }
-
-    inputElem.addEventListener('input', (_e) =>
-        updateRemainingCharacters(maxlength, inputElem, inputHint)
+    displayInitialHintText(commonParams);
+    inputElem.addEventListener('input', (_event) =>
+        updateHintText(commonParams)
     );
 }
 
-function updateRemainingCharacters(maxlength, inputElem, inputHint) {
-    const charactersRemaining = maxlength - inputElem.value.length;
+function displayInitialHintText(commonParams) {
+    const { charLimit, inputElem, inputHint } = commonParams;
 
-    if (charactersRemaining < 0) {
-        inputHint.classList.replace('govuk-hint', 'govuk-error-message');
-        inputHint.textContent = `You have ${Math.abs(
-            charactersRemaining
-        )} characters too many`;
-    } else {
-        inputHint.classList.contains('govuk-error-message') &&
-            inputHint.classList.replace('govuk-error-message', 'govuk-hint');
-        inputHint.textContent = `You have ${charactersRemaining} characters remaining`;
-    }
+    inputElem.value.length > 0
+        ? updateHintText(commonParams)
+        : (inputHint.textContent = `You have ${charLimit} characters remaining`);
+}
+
+function updateHintText({ charLimit, inputElem, inputHint }) {
+    const charactersRemaining = charLimit - inputElem.value.length;
+    const overCharLimit = charactersRemaining < 0;
+
+    overCharLimit
+        ? showHintError(inputHint, charactersRemaining)
+        : showRemainingCharacters(inputHint, charactersRemaining);
+}
+
+function showHintError(inputHint, charactersRemaining) {
+    inputHint.classList.replace('govuk-hint', 'govuk-error-message');
+    inputHint.textContent = `You have ${Math.abs(
+        charactersRemaining
+    )} characters too many`;
+}
+
+function showRemainingCharacters(inputHint, charactersRemaining) {
+    inputHint.classList.contains('govuk-error-message') &&
+        inputHint.classList.replace('govuk-error-message', 'govuk-hint');
+    inputHint.textContent = `You have ${charactersRemaining} characters remaining`;
 }
