5cdd546e31d13a020d743326295fc7531fdecbb1
Richard Bray <richardbray89@gmail.com>
2021-11-23 16:47:20 +0000

    feat: add ability to download receipt

---
+++ api/controllers/ReceiptDownloadController.js
@@ -0,0 +1,48 @@
+const sails = require('sails');
+const stream = require('stream');
+const util = require('util');
+const CasebookService = require('../services/CasebookService');
+
+const ReceiptDownloadController = {
+    async getReceipt(req, res) {
+        try {
+            await ReceiptDownloadController._streamReceiptToClient(req, res);
+        } catch (err) {
+            sails.log.error(err);
+            return res.serverError();
+        }
+    },
+
+    async _streamReceiptToClient(req, res) {
+        try {
+            ReceiptDownloadController._errorChecks(req, res);
+            sails.log.info('Downloading receipt from Casebook');
+
+            const streamFinished = util.promisify(stream.finished);
+            const applicationRef = req.params.applicationRef;
+            const response = await CasebookService.getApplicationReceipt(
+                applicationRef
+            );
+            response.data.pipe(res);
+
+            return streamFinished(res);
+        } catch (err) {
+            throw new Error(`ReceiptDownloadController Error: ${err}`);
+        }
+    },
+
+    _errorChecks(req, res) {
+        if (req.params.applicationRef === 'undefined') {
+            throw new Error('Missing application reference');
+        }
+
+        const userData = HelperService.getUserData(req, res);
+        if (!userData.loggedIn) {
+            throw new Error('User is not logged in');
+        }
+    },
+};
+
+module.exports = {
+    getReceipt: ReceiptDownloadController.getReceipt,
+};
