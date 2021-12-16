296817801fd4d565c8980a4d21787bd1933ecc78
Richard Bray <richardbray89@gmail.com>
2021-12-16 14:53:54 +0000

    refactor: remove commented out code

--- api/helpers/uploadedFileErrorChecks.js
+++ api/helpers/uploadedFileErrorChecks.js
@@ -82,17 +82,6 @@ async function scanStreamOfS3File(file, req) {
     try {
         const storageName = getStorageNameFromSession(file, req);
         const s3Bucket = req._sails.config.upload.s3_bucket;
-        // const fileStream = s3
-        //     .getObject({
-        //         Bucket: req._sails.config.upload.s3_bucket,
-        //         Key: getStorageNameFromSession(file, req),
-        //     })
-        //     .createReadStream()
-        //     .on('error', (error) => {
-        //         throw new Error(error);
-        //     })
-        //     .on('end', () => resolve());
-
         const scanResults = await clamscan.scanStream(
             getS3FileStream(storageName, s3Bucket)
         );
@@ -109,20 +98,16 @@ async function scanStreamOfS3File(file, req) {
 }
 
 function getS3FileStream(storageName, s3Bucket) {
-    try {
-        return s3
-            .getObject({
-                Bucket: s3Bucket,
-                Key: storageName,
-            })
-            .createReadStream()
-            .on('error', (error) => {
-                throw new Error(error);
-            })
-            .on('end', () => resolve());
-    } catch (error) {
-        throw new Error(error);
-    }
+    return s3
+        .getObject({
+            Bucket: s3Bucket,
+            Key: storageName,
+        })
+        .createReadStream()
+        .on('error', (error) => {
+            throw new Error(error);
+        })
+        .on('end', () => resolve());
 }
 
 function displayFileTypeErrorAndDeleteFile(file, req, fileType) {
