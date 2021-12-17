2c221ea95f7b11148c12bb36c899692857defc48
Richard Bray <richardbray89@gmail.com>
2021-12-17 18:07:32 +0000

    feat: change upload spinner to progress bar

--- assets/js/uploadFileSpinner.js
+++ assets/js/uploadFileSpinner.js
@@ -1,30 +1,92 @@
-var UploadFileSpinner = {
-    init: function() {
-        var buttonElem = document.querySelector(
-            '.js-trigger-spinner'
-        );
+var timeStarted = 0;
+var totalFilesToUpload = 0;
+var totalBytesToUpload = 0;
+var totalBytesUploaded = 0;
 
-        buttonElem.addEventListener('click', function (_event) {
+var UploadFileSpinner = {
+    init: function () {
+        var uploadBtn = document.querySelector('.js-trigger-progress-bar');
+
+        uploadBtn.addEventListener('click', function (_event) {
             var hasSelectedFiles = UploadFileSpinner.checkFilesSelected();
 
             if (hasSelectedFiles) {
-                UploadFileSpinner.showSpinnerHideContent();
+                timeStarted = new Date();
+                var formData = UploadFileSpinner.createDataForForm();
+                UploadFileSpinner.pretendToSendFormData(formData);
+                UploadFileSpinner.hideButtonShowProgressBar();
             }
         });
     },
 
-    checkFilesSelected: function() {
+    checkFilesSelected: function () {
         var fileInput = document.querySelector('.js-multi-file-input');
         return fileInput.files.length > 0;
     },
 
-    showSpinnerHideContent: function() {
-        var content = document.querySelector('.js-upload-content-wrapper');
-        var spinner = document.querySelector('.js-upload-spinner-wrapper');
+    createDataForForm: function () {
+        var uploadInput = document.querySelector('.js-multi-file-input');
+        var formData = new FormData();
+        var fileListArr = Array.from(uploadInput.files);
+        totalFilesToUpload = uploadInput.files.length;
 
-        content.classList.add('govuk-!-display-none');
-        spinner.classList.remove('govuk-!-display-none');
+        fileListArr.forEach(function (file, index) {
+            formData.append('file' + index, file);
+        });
+
+        return formData;
     },
-}
+
+    pretendToSendFormData: function (formData) {
+        var request = new XMLHttpRequest();
+        var emptyPostRequest = '';
+
+        request.upload.addEventListener('progress', function (event) {
+            var progressBar = document.querySelector('.js-upload-progress-bar');
+            var progressPct = (event.loaded / event.total) * 100;
+
+            progressBar.value = Math.round(progressPct);
+            totalBytesToUpload = event.total;
+            totalBytesUploaded = event.loaded;
+        });
+
+        request.open('POST', emptyPostRequest);
+        request.send(formData);
+    },
+
+    displayTimeRemaining: function (timeRemainingInSeconds) {
+        var secondsRemainingElem = document.querySelector(
+            '.js-upload-seconds-remaining'
+        );
+        var secondsStr = 'seconds';
+        if (timeRemainingInSeconds === 1) {
+            secondsStr = 'second';
+        }
+        secondsRemainingElem.innerHTML =
+            timeRemainingInSeconds + ' ' + secondsStr + ' remaining';
+    },
+
+    hideButtonShowProgressBar: function() {
+        var uploadBtn = document.querySelector('.js-upload-btn');
+        var progressBar = document.querySelector('.js-progress-bar');
+        uploadBtn.classList.add('govuk-!-display-none');
+        progressBar.classList.remove('govuk-!-display-none');
+    },
+};
+
+/**
+ * @ref https://stackoverflow.com/a/21163574/2395062
+ */
+setInterval(function () {
+    if (timeStarted !== 0) {
+        var timeElapsed = new Date() - timeStarted;
+        var uploadSpeed = totalBytesUploaded / (timeElapsed / 1000);
+        var timeRemainingInSeconds =
+            (totalBytesToUpload - totalBytesUploaded) / uploadSpeed;
+        UploadFileSpinner.displayTimeRemaining(
+            Math.round(timeRemainingInSeconds / 10)
+        );
+    }
+}, 1000);
 
 UploadFileSpinner.init();
