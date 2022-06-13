d2ea72b4ae07ba72751afa70e856cad0c6af29f7
Richard Oliver Bray <richardbray89@gmail.com>
2022-06-13 17:38:34 +0100

    refactor: add error handling to promises

--- api/controllers/EAppStartPageController.js
+++ api/controllers/EAppStartPageController.js
@@ -1,12 +1,15 @@
 const Application = require('../models/index').Application;
 const ApplicationReference = require('../models/index').ApplicationReference;
 
+const eAppServiceNum = 4;
+
 const EAppStartPageController = {
     startPage(req, res) {
         const userData = HelperService.getUserData(req, res);
-        const backUrl = req.query.arrivedFrom === 'skipQuestionsLink'
-            ? '/eligibility/check-documents-are-eligible'
-            : '/eligibility/check-documents-are-prepared';
+        const backUrl =
+            req.query.arrivedFrom === 'skipQuestionsLink'
+                ? '/eligibility/check-documents-are-eligible'
+                : '/eligibility/check-documents-are-prepared';
 
         return res.view('eApostilles/startPage.ejs', {
             user_data: userData,
@@ -15,56 +18,68 @@ const EAppStartPageController = {
     },
 
     async startElectronicApplication(req, res) {
-        const userData = HelperService.getUserData(req, res);
-        const signInUrl = `${sails.config.customURLs.userServiceURL}/sign-in?from=start&next=continueEApp`;
-        const nextUrl = userData.loggedIn ? '/upload-files': signInUrl;
+        try {
+            const userData = HelperService.getUserData(req, res);
+            const signInUrl = `${sails.config.customURLs.userServiceURL}/sign-in?from=start&next=continueEApp`;
+            const nextUrl = userData.loggedIn ? '/upload-files' : signInUrl;
 
-        const appIdExistsInSession = Boolean(req.session.appId);
-        const appTypeExistsInSession = Boolean(req.session.appType);
-        const appDetailsInSession = appIdExistsInSession && appTypeExistsInSession;
+            const appIdExistsInSession = Boolean(req.session.appId);
+            const appTypeExistsInSession = Boolean(req.session.appType);
+            const appDetailsInSession =
+                appIdExistsInSession && appTypeExistsInSession;
 
-        if (!appDetailsInSession) {
-            const newAppData = await EAppStartPageController._createNewApplicationInDB();
-            EAppStartPageController._addApplicationDetailsToSession(req, newAppData);
-            EAppStartPageController._wipePreviousSessionVariables(req);
+            if (!appDetailsInSession) {
+                const newAppData =
+                    await EAppStartPageController._createNewApplicationInDB();
+                EAppStartPageController._addApplicationDetailsToSession(
+                    req,
+                    newAppData
+                );
+                EAppStartPageController._wipePreviousSessionVariables(req);
+            }
+
+            return res.redirect(nextUrl);
+        } catch (err) {
+            sails.log.error(err);
+            return res.serverError();
         }
-
-        return res.redirect(nextUrl);
     },
 
     async _createNewApplicationInDB() {
-        const appRefDetails = await ApplicationReference.findOne();
-        const eAppServiceNum = 4;
-        const uniqueApplicationId =
-        HelperService.generateNewApplicationId(
-            appRefDetails,
-            eAppServiceNum.toString()
-        );
-        const newAppInDB = await Application.create({
-            serviceType: eAppServiceNum,
-            unique_app_id: uniqueApplicationId,
-            all_info_correct: '-1',
-            user_id: 0,
-            submitted: 'draft',
-            company_name: 'N/A',
-            feedback_consent: 0,
-            doc_reside_EU: 0,
-            residency: 0,
-        });
+        try {
+            const appRefDetails = await ApplicationReference.findOne();
+            const uniqueApplicationId = HelperService.generateNewApplicationId(
+                appRefDetails,
+                eAppServiceNum.toString()
+            );
+            const newAppInDB = await Application.create({
+                serviceType: eAppServiceNum,
+                unique_app_id: uniqueApplicationId,
+                all_info_correct: '-1',
+                user_id: 0,
+                submitted: 'draft',
+                company_name: 'N/A',
+                feedback_consent: 0,
+                doc_reside_EU: 0,
+                residency: 0,
+            });
 
-        return newAppInDB;
+            return newAppInDB;
+        } catch (err) {
+            throw new Error(`_createNewApplicationInDB: ${err}`);
+        }
     },
 
     _addApplicationDetailsToSession(req, newAppInDB) {
-        req.session.appId = newAppInDB.dataValues.application_id;;
-        req.session.appType = 4;
+        req.session.appId = newAppInDB.dataValues.application_id;
+        req.session.appType = eAppServiceNum;
     },
 
     _wipePreviousSessionVariables(req) {
         req.session.selectedDocs = '';
         req.session.return_address = '';
         req.session.appSubmittedStatus = false;
-    }
-}
+    },
+};
 
 module.exports = EAppStartPageController;
