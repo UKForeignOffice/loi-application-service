9facfe56ecaa107c6c8cad84da92fab5b353bdd3
Simon Moore <s.moore@kainos.com>
2022-08-23 15:20:30 +0100

    Reverse css caching change

--- tasks/config/sass.js
+++ tasks/config/sass.js
@@ -3,7 +3,6 @@
  */
 
 module.exports = function(grunt) {
-  const stringToPreventCaching = new Date().getTime();
 
     grunt.config.set('sass', {
         dev: {
@@ -15,7 +14,7 @@ module.exports = function(grunt) {
                 cwd: 'assets/styles/',
                 src: ['importer.scss'],
                 dest: '.tmp/public/styles/',
-                ext: `.${stringToPreventCaching}.css`
+                ext: '.css'
             }]
         }
     });
