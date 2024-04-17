79af9dfee147c05ef8e9a1d9252141834c3ee002
Simon Moore <s.moore@kainos.com>
2024-04-17 15:20:31 +0100

    Update sql for new courier pricing

---
+++ database_change_log/2024-05-06__update_courier_pricing.sql
@@ -0,0 +1,3 @@
+update "PostagesAvailable" set "price" = 5.50 where "pretty_title" = 'UK courier delivery';
+update "PostagesAvailable" set "price" = 22.50 where "title" = 'European Courier';
+update "PostagesAvailable" set "price" = 26.50 where "title" = 'International Courier';
