cd93713372ec64dfb8e7b176efc254cb06923b7f
Alistair Laing <aliuk2012@users.noreply.github.com>
2026-05-28 16:58:52 +0100

    Remove jshint
    We are now using biome.js for formating and linting
    
    Signed-off-by: Alistair Laing <aliuk2012@users.noreply.github.com>

--- tasks/config/jshint.js
+++
@@ -1,8 +0,0 @@
-module.exports = (grunt) => {
-  grunt.config.set('jshint', {
-    all: ['api/**/*.js'],
-    //'test/**/*.js', 'data/**/*.js']
-  })
-
-  grunt.loadNpmTasks('grunt-contrib-jshint')
-}
