e9eb45a231d2acfa19faa899c427e4cd5270165d
Alistair Laing <aliuk2012@users.noreply.github.com>
2026-06-02 14:54:38 +0100

    Remove old test dependencies
    Signed-off-by: Alistair Laing <aliuk2012@users.noreply.github.com>

--- .mocharc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  file: './tests/bootstrap.test.js',
-  spec: './tests/specs/**/*.test.js',
-  ignore: ['./tests/vitest/**/*.test.js'],
-  timeout: '10000',
-  recursive: true,
-  exit: true,
-}
