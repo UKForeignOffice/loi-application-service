f921c26c29807f04155f33c43b35188e223c1448
Simon Moore <s.moore@kainos.com>
2026-09-18 13:45:47 +0100

    Adding script to update country names

---
+++ database_change_log/2026_09_18__leg80_country_name_updates.sql
@@ -0,0 +1,19 @@
+UPDATE country
+SET name = 'Naoero', casebook_mapping = 'Naoero'
+WHERE name = 'Nauru';
+
+UPDATE country
+SET name = 'Sint Maarten', casebook_mapping = 'Sint Maarten'
+WHERE name = 'St Maarten';
+
+UPDATE country
+SET name = 'Bonaire, Sint Eustatius and Saba', casebook_mapping = 'Bonaire, Sint Eustatius and Saba'
+WHERE name = 'Bonaire/St Eustatius/Saba';
+
+UPDATE country
+SET name = 'The Bahamas', casebook_mapping = 'The Bahamas'
+WHERE name = 'The Bahamas';
+
+UPDATE country
+SET name = 'The Gambia', casebook_mapping = 'The Gambia'
+WHERE name = 'The Gambia';
